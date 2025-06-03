02d3c2a041f78efa67bf0e905c9a1f23b7160a04
GabrielFaria1509 <fariagabriel1509@gmail.com>
2025-06-02 23:23:10 -0300

    Create Q3.cpp

---
+++ Q3.cpp
@@ -0,0 +1,173 @@
+#include <iostream>
+
+using namespace std;
+
+// Funções para Árvore Binária de Busca ---------------------------------------
+
+typedef int KeyType;  //número da torta
+typedef int ValueType; //preço final da torta
+
+struct DataType {
+	KeyType key;
+	ValueType value;
+};
+
+struct Node {
+	DataType data;
+	Node * right;
+	Node * left;
+} ;
+
+
+void insert(Node *& curr, DataType data){	
+	if(curr == NULL){
+		curr = new Node;
+		curr->data = data;
+		curr->right = NULL;
+		curr->left = NULL;
+	} else {
+		if(data.value >= curr->data.value){
+			insert(curr->right, data);
+		} else if(data.value < curr->data.value) {
+			insert(curr->left, data);
+		}		
+	}	
+}
+
+Node * search(Node * curr, KeyType key){
+	if(curr == NULL)
+		return NULL;
+	if(key == curr->data.key){
+		return curr;
+	} else {
+		if(key > curr->data.key){
+			return search(curr->right, key);
+		} else if(key < curr->data.key){
+			return search(curr->left, key);
+		}
+	}
+	return NULL;
+}
+
+void show_ordered(Node * curr){
+	if(curr->left != NULL)
+		show_ordered(curr->left);
+	cout << curr->data.key << ":" << curr->data.value << endl;
+	if(curr->right != NULL)
+		show_ordered(curr->right);
+}
+
+Node * disconnect_lesser(Node *& curr){
+	Node * aux = curr;
+	if(aux->left == NULL){ 	
+		curr = curr->right; 
+		return aux;			
+	} else {				
+		return disconnect_lesser(curr->left);
+	}
+}
+
+
+int remove(Node *& curr, KeyType old_key){
+	if(curr == NULL){
+		return 0;
+	}
+	Node * aux;
+	if(curr->data.key == old_key){
+		aux = curr;		
+		if(curr->left == NULL){
+			curr = aux->right;
+		} else if(curr->right == NULL){
+			curr = aux->left;
+		} else {
+			aux = disconnect_lesser(curr->right);
+			curr->data = aux->data;
+		}		
+		delete aux;
+		return 1;		
+	} else {
+		if(old_key > curr->data.key){
+			return remove(curr->right, old_key);
+		} else if(old_key < curr->data.key) {
+			return remove(curr->left, old_key);
+		}
+	}
+	return 0;
+}
+
+
+void destruct(Node *& curr){
+	if(curr->left != NULL)
+		destruct(curr->left);
+	if(curr->right != NULL)
+	   	destruct(curr->right);
+	delete curr;
+	curr = NULL;
+	return;
+}
+
+// ----------------------------------------------------------------------------
+
+
+typedef struct {
+	int x;
+	int y;	
+} Ponto;
+
+double area_triangulo(
+	Ponto a, 
+	Ponto b, 
+	Ponto c
+){
+	return 
+		((a.x*b.y) - (a.y*b.x) + 
+		(a.y*c.x) - (a.x*c.y) + 
+		(b.x*c.y) - (b.y*c.x))/2.0;  
+}
+
+double distancia(Ponto p1, Ponto p2)
+{
+    return sqrt((p1.x - p2.x)*(p1.x - p2.x) +
+          (p1.y - p2.y)*(p1.y - p2.y));
+}
+
+int orientacao(Ponto a, Ponto b, Ponto c){
+	double area = area_triangulo(a,b,c);
+	if(area > 0){
+		return 1; 
+	} else if(area < 0){
+		return -1; 
+	} else {
+		return 0; 
+	}
+}
+
+int main(){
+    int t; //número de tortas
+    DataType torta //struct para torta //torta é um tipo de dado
+    Node*root = NULL; //inclusão da árvore , começa vazia
+    Ponto a,b,c,d;  //chamada struct de pontos
+    int em; //espessura da massa
+    int a; //altura da torta
+    int d; //diametro da porta
+    int p; //peso torta
+    int pfmin;
+    int pfmax;
+    
+    cin >> t;
+    
+    for(int i = 0; i < t; i++){
+        cin >> a.x >> a.y;
+        cin >> b.x >> b.y;
+        cin >> c.x >> c.y;
+        cin >> d.c >> d.y;
+        
+        em = distancia(a,b);
+        d = distancia(c,d);
+        a = distancia(a,c);
+        
+        
+    }
+    
+
+}
